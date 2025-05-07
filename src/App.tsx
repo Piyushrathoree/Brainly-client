44ff5fcb106875bee6b2fd184827a0d5a5270109
Piyushrathoree <meonpath008@gmail.com>
2025-05-07 19:15:29 +0530

    fix: remove trailing whitespace in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div className="h-screen w-screen flex flex-col items-start bg-white dark:bg-black/97 justify-start overflow-hidden">
         <Navbar2 />
-        <MainSection />
+        <MainSection /> 
       </div>
     </>
   );
